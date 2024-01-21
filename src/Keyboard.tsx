076d185a4fa478a9c87b9632a10d4506ac496426
samlaconico <99830026+samlaconico@users.noreply.github.com>
2024-01-21 02:13:51 -0800

    slight changes to the keyboard

--- src/Keyboard.tsx
+++ src/Keyboard.tsx
@@ -46,7 +46,7 @@ export function Keyboard({
     <div
       style={{
         display: "grid",
-        gridTemplateColumns: "repeat(auto-fit, minmax(75px, 1fr))",
+        gridTemplateColumns: "repeat(auto-fit, minmax(50px, 1fr))",
         gap: ".5rem",
       }}
     >
